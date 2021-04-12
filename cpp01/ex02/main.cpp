ba48aefa36d496ad2741c2844f705ddbe6f2f62a
kuokka <audiotrack8619@gmail.com>
2021-04-12 13:54:15 +0900

    feat : add ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seunghoh <seunghoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 16:42:53 by seunghoh          #+#    #+#             */
-/*   Updated: 2021/04/09 17:43:18 by seunghoh         ###   ########.fr       */
+/*   Updated: 2021/04/12 13:52:20 by seunghoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,20 @@
 
 int		main()
 {
-	Zombie* z[100];
+	Zombie* z[4];
 	ZombieEvent	event;
 
-	z[0] = event.newZombie("seunghoh");
+	z[0] = new Zombie("trump", "crazy");
 	z[0]->announce();
-	z[1] = event.randomChump();
-	z[2] = event.randomChump();
+	z[1] = event.newZombie("seunghoh");
+	z[1]->announce();
+	event.setZombieType("corona");
+	z[2] = event.newZombie("ping");
+	z[2]->announce();
 	z[3] = event.randomChump();
-	z[4] = event.randomChump();
-	z[5] = event.randomChump();
+	delete z[0];
+	delete z[1];
+	delete z[2];
+	delete z[3];
 	return (0);
 }
