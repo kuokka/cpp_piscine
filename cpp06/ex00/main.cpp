184fb4ac9b03002a6e4bb65ded5f65b7401774f3
kuokka <audiotrack8619@gmail.com>
2021-06-01 03:04:46 +0900

    [feat] : cpp06 ex00

---
+++ cpp06/ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunghoh <seunghoh@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/01 02:04:31 by seunghoh          #+#    #+#             */
+/*   Updated: 2021/06/01 03:01:14 by seunghoh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include "Converter.hpp"
+
+int		main(int argc, char **argv)
+{
+	std::string	literal;
+
+	if (argc != 2)
+	{
+		std::cout << "Invalid Arguments" << std::endl;
+		return (1);
+	}
+	literal = std::string(argv[1]);
+	Converter	converter(literal);
+	converter.toChar();
+	converter.toInt();
+	converter.toFloat();
+	converter.toDouble();
+	std::cout << static_cast<float>('a') << std::endl;
+	return (0);
+}
