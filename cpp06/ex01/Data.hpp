f73c1a4d2080329a5d9261e665cc3ba997e5374b
kuokka <audiotrack8619@gmail.com>
2021-05-31 22:25:32 +0900

    [feat] : ex01 complete

--- cpp06/ex01/Data.hpp
+++ cpp06/ex01/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: seunghoh <seunghoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 19:59:03 by seunghoh          #+#    #+#             */
-/*   Updated: 2021/05/31 21:00:13 by seunghoh         ###   ########.fr       */
+/*   Updated: 2021/05/31 22:23:43 by seunghoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,18 @@
 class	Data
 {
 	private :
-		std::string	s1_;
+		char		*s1_;
 		int			n_;
-		std::string	s2_;
+		char		*s2_;
+		Data();
 
 	public :
-		Data(const std::string& s1, int n, const std::string& s2);
+		Data(char *s1, int n, char *s2);
 		Data(const Data& ref);
 		Data&	operator=(const Data& ref);
 		~Data();
-		std::string	getS1() const;
-		std::string	getS2() const;
+		char*	getS1() const;
+		char*	getS2() const;
 		int	getN() const;
 };
 
